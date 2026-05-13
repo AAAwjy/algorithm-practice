e9aec2b5b46f03d7b293aa11f8d6c37bf652480a
AAAwjy <2191198424@qq.com>
2026-05-13 15:06:27 +0800

    Add deleteNode function for binary search tree
    Implement deleteNode function to remove a node from a binary search tree.
    
    Signed-off-by: AAAwjy <2191198424@qq.com>

---
+++ deleteNode.cpp
@@ -0,0 +1,32 @@
+public:
+TreeNode* deleteNode(TreeNode* root, int key) {
+    if(!root) return root;
+    if(key<root->val)
+        root->left=deleteNode(root->left,key);
+    else if(key>root->val)
+        root->right=deleteNode(root->right,key);
+    else if(root->val==key){
+        //1.叶子节点
+        if(!root->left&&!root->right) return nullptr;
+        //2.左空右不空
+        if(!root->left&&root->right) return root->right;
+        //3.左不空右空
+        if(root->left&&!root->right) return root->left;
+        //4.左不空右不空
+        if(root->left&&root->right) {
+            //选择左节点作为替换节点,左节点的右子树要接到右节点的左子树上
+            TreeNode* rl=Findrl(root->right);
+            rl->left=root->left->right;
+            root->left->right=root->right;
+            return root->left;
+        }
+    }
+    return root;
+}
+
+TreeNode* Findrl(TreeNode* root){
+    while(root->left){
+        root=root->left;
+    }
+    return root;
+}
